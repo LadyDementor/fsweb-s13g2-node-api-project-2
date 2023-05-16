c2c0e1cd2bf3443432915ddc2953723b582f10e3
LadyDementor <125830907+LadyDementor@users.noreply.github.com>
2023-05-17 01:55:47 +0300

    S13g2 projesi

--- api/server.js
+++ api/server.js
@@ -1,3 +1,10 @@
-// server için gerekli olanları burada ayarlayın
+const express = require("express");
+const server = express();
+const postRouter = require("./posts/posts-router");
+server.use(express.json()); //json istekleri desteklemesi için.
+
+server.use("/api/posts", postRouter);
 
 // posts router'ını buraya require edin ve bağlayın
+
+module.exports = server;
